a6d921ef8f0ba460552d0bf0dedd19bd639cd596
Thiago Rocha <thiagodesousarocha1311@gmail.com>
2022-12-17 12:27:19 -0300

    first commit

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer center">
+        <p>© 2022 Óticas Vida - Todos os direitos reservados. - Dev. by <a href="https://dithan.github.io" target='_blank'>Thiago Rocha</a></p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
